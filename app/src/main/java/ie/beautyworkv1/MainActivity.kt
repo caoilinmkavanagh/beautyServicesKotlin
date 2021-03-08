38b847d06ab3aedcc002dcde21ced22fe84615e3
caoilink <caoilinkavanagh@gmail.com>
2021-03-08 16:00:10 +0000

    Initial commit

---
+++ app/src/main/java/ie/beautyworkv1/MainActivity.kt
@@ -0,0 +1,11 @@
+package ie.beautyworkv1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
